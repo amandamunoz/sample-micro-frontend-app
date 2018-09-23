a553000bd9d3509c914e5374c2bd9756b1e9c8a8
Amanda Muñoz <amandag.munoz@gmail.com>
2018-09-23 17:06:37 -0400

    Initial commit from create-react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div id="react">
+                This is a react app!
+            </div>
+        );
+    }
+}
+
+export default App;
